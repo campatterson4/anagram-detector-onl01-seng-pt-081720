66cca9c0125dc98babd1bb19933e5fd82bf4791e
Camryn Patterson <camrynpatterson@yahoo.com>
2020-09-11 19:45:16 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,20 +1,16 @@
 class Anagram
+attr_accessor :word
   
-  attr_accessor :word
+def initialize(word)
+@word = word
+end
   
-  def initialize(word)
-    @word = word
-  end
-  
-  def match(array_of_words)
+def match(array_of_words)
 
-    
-    array_of_words.select do |element|
-      (@word.split("").sort) == (element.split("").sort)
-    end
-  end
-  
-  
+array_of_words.select do |element|
+(@word.split("").sort) == (element.split("").sort)
+end
+end
 end
 
 listen = Anagram.new("listen")
